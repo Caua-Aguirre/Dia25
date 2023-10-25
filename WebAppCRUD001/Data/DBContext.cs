02d7b7544173a256b995c3bce5f004fc27d0a058
CAUA SANTOS AGUIRRE <caua.saguirre@senacsp.edu.br>
2023-10-25 17:11:45 -0300

    commit fim de aula

---
+++ WebAppCRUD001/Data/DBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAppCRUD001.Models;
+
+namespace WebAppCRUD001.Data
+{
+    public class DBContext : DbContext
+    {
+        public DBContext (DbContextOptions<DBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAppCRUD001.Models.Consulta> Consulta { get; set; } = default!;
+    }
+}
--- WebAppCRUD001/Models/Consulta.cs
+++ WebAppCRUD001/Models/Consulta.cs
@@ -2,5 +2,8 @@
 {
     public class Consulta
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Sobrenome { get; set;}
     }
 }
